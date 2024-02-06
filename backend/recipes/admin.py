02146f1a442545d0fd57d4c4b1c8753e1919869f
lengenkon <92142482+lengenkon@users.noreply.github.com>
2024-02-06 10:14:55 +0300

    Update admin.py

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,8 +1,6 @@
 from itertools import chain
 
 from django.contrib import admin
-from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin
 from users.models import Follow
 
 from .models import (Favorites, Ingredient, IngredientIndividual, Recipe,
