44e64067bba9f017ea2652da21fa1013a3867bb0
lengenkon <92142482+lengenkon@users.noreply.github.com>
2024-02-06 13:42:24 +0300

    Update serializers.py

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -10,7 +10,29 @@ from rest_framework.validators import UniqueTogetherValidator
 from users.models import Follow
 from users.serializers import CustomUserSerializer
 
+from django.contrib.auth import get_user_model
+from djoser.serializers import UserSerializer
+from rest_framework import serializers
+from users.models import Follow
+
 User = get_user_model()
+
+class CustomUserSerializer(UserSerializer):
+    is_subscribed = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ('email', 'id', 'username',
+                  'first_name', 'last_name',
+                  'is_subscribed')
+
+    def get_is_subscribed(self, obj):
+        if self.context['request'].user.is_authenticated:
+            current_user = User.objects.get(
+                username=self.context['request'].user)
+            return Follow.objects.filter(
+                following=obj.id, user=current_user.id).exists()
+        return False
 
 
 class TagSerializer(serializers.ModelSerializer):
