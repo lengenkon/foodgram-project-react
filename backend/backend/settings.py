b789ac39bebf7e9c3645a57e4138f7b7ff6e85e9
Lena Konstantinova <smile711@mail.ru>
2024-02-19 22:29:24 +0300

    Debug false

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key)
 
 DEBUG = os.getenv('DEBUG') == 'True'
-DEBUG = True
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split()
 
